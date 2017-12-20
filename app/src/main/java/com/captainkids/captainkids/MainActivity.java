35e2094f72d54cf37ab313154ea24c16dc66c77a
Camille Basset <camille.basset@hotmail.fr>
2017-12-20 17:02:48 +0100

    fix sign-in page

--- app/src/main/java/com/captainkids/captainkids/MainActivity.java
+++ app/src/main/java/com/captainkids/captainkids/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button connect = (Button) findViewById(R.id.connect);
+        Button connect = (Button) findViewById(R.id.signinbtn);
 
         connect.setOnClickListener(new View.OnClickListener() {
             @Override
