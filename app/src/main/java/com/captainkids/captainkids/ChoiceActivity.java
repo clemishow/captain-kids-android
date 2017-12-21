46bdbf2d550c192ebc1d3c09b6d5ad5575cf41d6
Anne MAURICE-PEROUMAL <anne.maurice-peroumal@hetic.net>
2017-12-21 17:11:40 +0100

    Bonjour user mis en place

--- app/src/main/java/com/captainkids/captainkids/ChoiceActivity.java
+++ app/src/main/java/com/captainkids/captainkids/ChoiceActivity.java
@@ -3,11 +3,22 @@ package com.captainkids.captainkids;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ResourceBundle;
+
 
 
 public class ChoiceActivity extends AppCompatActivity  {
+
+    private TextView TextViewUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +27,19 @@ public class ChoiceActivity extends AppCompatActivity  {
 
         Bundle bundle = getIntent().getExtras();
         Button connect = (Button) findViewById(R.id.accompagnerbtn);
+
+        TextViewUsername = (TextView) findViewById(R.id.TextViewUsername);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+
+            String uid = user.getUid();
+
+            TextViewUsername.setText("Bonjour " + name);
+        }
 
         connect.setOnClickListener(new View.OnClickListener() {
             @Override
