591c9e83cbee68dcc12fe8e74c7b393425f4e18e
Anne MAURICE-PEROUMAL <anne.maurice-peroumal@hetic.net>
2017-12-21 14:00:29 +0100

    Conflicts fix

--- app/src/main/java/com/captainkids/captainkids/ChoiceActivity.java
+++ app/src/main/java/com/captainkids/captainkids/ChoiceActivity.java
@@ -1,7 +1,10 @@
 package com.captainkids.captainkids;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 
 public class ChoiceActivity extends AppCompatActivity {
@@ -12,6 +15,20 @@ public class ChoiceActivity extends AppCompatActivity {
         setContentView(R.layout.activity_choice);
 
         Bundle bundle = getIntent().getExtras();
+        Button connect = (Button) findViewById(R.id.accompagnerbtn);
 
+        connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPage4();
+            }
+        });
     }
+
+    private void openPage4() {
+        Intent intent = new Intent(this, Main4Activity.class);
+        startActivity(intent);
+    }
+
+
 }
